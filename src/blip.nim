ac0e971114f94c095486c04912608b045187de92
Jens Alfke <jens@couchbase.com>
2020-06-18 18:01:44 -0700

    Added Apache 2 license

--- src/blip.nim
+++ src/blip.nim
@@ -1,5 +1,18 @@
-# This is just an example to get you started. A typical hybrid package
-# uses this file as the main entry point of the application.
+# blip.nim
+#
+# Copyright (c) 2020 Couchbase, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import blip/[message, outbox, protocol, transport]
 import blip/private/[crc32, log, varint]
